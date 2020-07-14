b2cf24c4ad6c6c0efe98caa6b4d8a0968fade976
DimoDM <dimo_marino@abv.bg>
2020-07-14 19:02:12 +0300

    configure

---
+++ src/main/java/facebook/config/WebSecurityConfiguration.java
@@ -0,0 +1,42 @@
+package facebook.config;
+
+import facebook.service.implementation.LoginServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final LoginServiceImpl userService;
+
+    @Autowired
+    public WebSecurityConfiguration(LoginServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/register").permitAll()
+//                    .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll()
+                .and()
+                .userDetailsService(userService)
+                .exceptionHandling().accessDeniedPage("/unauthorized")
+                .and()
+                .csrf().disable();
+    }
+
+}
