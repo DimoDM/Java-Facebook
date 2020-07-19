9f7d324aaa4687f23e02bd07d0a650ffa2c0b928
Andrey-Hristov <andimpmchilov@gmail.com>
2020-07-19 16:56:26 +0300

    RegisterController changed

--- src/main/java/facebook/controller/RegisterController.java
+++ src/main/java/facebook/controller/RegisterController.java
@@ -1,16 +1,12 @@
 package facebook.controller;
 
-
 import facebook.dto.RegisterDTO;
-import facebook.entity.UserLoginData;
-import facebook.service.contract.RegisterService;
 import facebook.service.implementation.RegisterServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,12 +19,32 @@ public class RegisterController extends BaseController {
         this.registerService = registerService;
     }
 
+//    @GetMapping("/register")
+//    public ModelAndView registerState(ModelAndView modelAndView){
+//        modelAndView.setViewName("index.html");
+//        return modelAndView;
+//    }
+
+
+    @GetMapping("/home")
+    public ModelAndView test(){
+        return send("index");
+    }
 
     @PostMapping("/register")
     public ModelAndView register(@ModelAttribute RegisterDTO registerDTO) {
 
 
         registerService.register(registerDTO);
-        return send("RegistrationFormPage.html");
+        return send("facebook");
     }
+
+    @GetMapping("/register")
+    public ModelAndView registerGet() {
+
+        return send("facebook");
+    }
+
+
+
 }
