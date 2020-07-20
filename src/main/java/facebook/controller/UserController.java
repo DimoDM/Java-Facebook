b796fdc48d97810d5a8871e5fc155e7f691e762a
PetyaTsvetkova <tsvetkova.petya@yahoo.com>
2020-07-20 23:44:25 +0300

    Update UserController.java

--- src/main/java/facebook/controller/UserController.java
+++ src/main/java/facebook/controller/UserController.java
@@ -51,5 +51,22 @@ public class UserController extends BaseController {
         return send("login");
     }
 
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        model.addAttribute("user", user);
+        return "update-user";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id, @Valid User user,
+                             BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "update-user";
+        }
+
 
 }
