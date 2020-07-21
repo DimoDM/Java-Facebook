975947f5a8d5ca666d14f9764c566ee94e486f15
PetyaTsvetkova <tsvetkova.petya@yahoo.com>
2020-07-21 14:37:32 +0300

    UserController get and postmapping, update form
    UserController - added @GetMapping("/edit/{id}") and @PostMapping("/update/{id}")
    Update Form html and css

--- src/main/java/facebook/controller/UserController.java
+++ src/main/java/facebook/controller/UserController.java
@@ -3,14 +3,17 @@ package facebook.controller;
 
 import facebook.dto.LoginDTO;
 import facebook.dto.RegisterDTO;
+import facebook.entity.User;
 import facebook.service.implementation.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
