f97b9a11bff136f2fc6b4f2ef99cd168871bc3aa
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2020-07-23 21:53:22 +0300

    You can comment with a photo

--- src/main/java/facebook/service/implementation/CommentServiceImpl.java
+++ src/main/java/facebook/service/implementation/CommentServiceImpl.java
@@ -11,13 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CommentServiceDTO implements CommentService {
+public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
 
     @Autowired
-    public CommentServiceDTO(CommentRepository commentRepository, PostRepository postRepository) {
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
     }
