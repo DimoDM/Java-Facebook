2cdfcb0552975471428da953973c83432b219f12
GoSh069 <gogogospodinov69@gmail.com>
2020-07-23 04:25:12 +0300

    poor try to images

--- src/main/java/facebook/service/implementation/UserServiceImpl.java
+++ src/main/java/facebook/service/implementation/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package facebook.service.implementation;
 
 import facebook.dto.RegisterDTO;
+import facebook.entity.Picture;
 import facebook.entity.Role;
 import facebook.entity.User;
 import facebook.entity.UserLoginData;
 import facebook.exception.UserNotFoundException;
+import facebook.repository.PictureRepository;
 import facebook.repository.UserLoginDataRepository;
 import facebook.repository.UserRepository;
 import facebook.service.contract.UserService;
@@ -27,14 +29,16 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final RoleService roleService;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final PictureRepository pictureRepository;
 
 
     @Autowired
-    public UserServiceImpl(UserLoginDataRepository userLoginDataRepository, UserRepository userRepository, RoleService roleService, BCryptPasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserLoginDataRepository userLoginDataRepository, PictureRepository pictureRepository, UserRepository userRepository, RoleService roleService, BCryptPasswordEncoder passwordEncoder) {
         this.userLoginDataRepository = userLoginDataRepository;
         this.userRepository = userRepository;
         this.roleService = roleService;
         this.passwordEncoder = passwordEncoder;
+        this.pictureRepository = pictureRepository;
     }
 
     @Transactional
@@ -55,6 +59,10 @@ public class UserServiceImpl implements UserService {
         user.setDateOfBirth(registerDTO.getDateOfBirth());
         user.setSecondName(registerDTO.getLastName());
         user.setGender(registerDTO.getGender());
+        Picture picture = new Picture();
+        picture.setImageURL("src/main/resources/static/images/avatar.png");
+        pictureRepository.save(picture);
+        user.setProfilePicture(picture);
         userLoginData.setEmail(registerDTO.getEmail());
         userLoginData.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
 
