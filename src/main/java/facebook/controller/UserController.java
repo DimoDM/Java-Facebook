fe48f15df8b089b50202415074184d44f59ca743
PetyaTsvetkova <tsvetkova.petya@yahoo.com>
2020-07-24 13:56:28 +0300

    Something small but hopefully relevant since I'm getting conflicts

--- src/main/java/facebook/controller/UserController.java
+++ src/main/java/facebook/controller/UserController.java
@@ -3,14 +3,18 @@ package facebook.controller;
 
 import facebook.dto.LoginDTO;
 import facebook.dto.RegisterDTO;
+import facebook.entity.User;
+import facebook.repository.UserRepository;
 import facebook.service.implementation.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -53,15 +57,36 @@ public class UserController extends BaseController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/facebook")
-    public ModelAndView test(){
+    public ModelAndView test() {
         return send("facebook");
     }
 
     @PreAuthorize("!isAuthenticated()")
     @GetMapping("/forgotPassword")
-    public ModelAndView forgottenPasswordPage(){
+    public ModelAndView forgottenPasswordPage() {
         return send("forgottenPasswordPage");
     }
 
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = UserRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
 
+        model.addAttribute("user", user);
+        return "updateUserInfoForm";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id, @Valid User user,
+                             BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "updateUserInfoForm";
+
+        }
+        UserRepository.save(user);
+        model.addAttribute("users", UserRepository.findAll());
+        return "redirect:/profile";
+
+    }
 }
